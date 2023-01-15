79885e6d03f9fabfa9437764cc1e6afebd8492b1
Hudson Q <hudson@koipond.xyz>
2023-01-14 19:45:51 -0600

    discord integration

--- ngrokgo.py
+++ ngrokgo.py
@@ -2,11 +2,14 @@ import time
 import os
 import linecache
 import re
+import requests
+
 log = 'ngrok.log'
 p = r'([a-zA-Z_][a-zA-Z_\d]*)=("[^"]*"|[^\s]+)'
+discord_url = "" #Optional. Place webhook here to send as a channel msg
 
 def exec():
-	os.system("ngrok start --all --log=stdout > ngrok.log &")
+	os.system("ngrok start --all --log=stdout > ngrok_ignore.log")
 	time.sleep(1)
 
 def parse_to_dict(s) -> dict:
@@ -15,14 +18,33 @@ def parse_to_dict(s) -> dict:
 		d[k] = v
 	return d
 
+def rokbot(x, y):
+	data = { 
+		"content" : x + "\n" + y,
+		"username" : "RokBot"
+	}
+	result = requests.post(discord_url, json = data)
+	try:
+		result.raise_for_status()
+	except requests.exceptions.HTTPError as err:
+		print(err)
+	else:
+		print("Payload delivered successfully, code {}.".format(result.status_code))
+
 def main():
 	exec()
+	#first app
 	info1 = linecache.getline(log, 8)
 	url1 = parse_to_dict(info1)["url"]
 	addr1 = parse_to_dict(info1)["addr"]
+	#second app
 	info2 = linecache.getline(log, 9)
 	url2 = parse_to_dict(info2)["url"]
 	addr2 = parse_to_dict(info2)["addr"]
-	print("tunnel " + url1 + " is exposed to " + addr1)
-	print("tunnel " + url2 + " is exposed to " + addr2)
+	#message vars 
+	status1 = "Tunnel " + url1 + " is exposed to " + addr1
+	status2 = "Tunnel " + url2 + " is exposed to " + addr2
+	#integration if webhook exists
+	if discord_url != "":
+		rokbot(status1, status2)
 main()
