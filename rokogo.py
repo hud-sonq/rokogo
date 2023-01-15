5f936debb99528542f1508944205e01a8158700c
Hudson Q <hudson@koipond.xyz>
2023-01-14 22:56:26 -0600

    small fixes

--- rokogo.py
+++ rokogo.py
@@ -6,18 +6,18 @@ import requests
 
 log = 'ngrok.log'
 p = r'([a-zA-Z_][a-zA-Z_\d]*)=("[^"]*"|[^\s]+)'
-discord_url = "" #Optional. Place webhook here to send as a channel msg
-
+discord_url = "" #Webhook here
+#Start ngrok and log
 def exec():
 	os.system("ngrok start --all --log=stdout > ngrok.log &")
 	time.sleep(1)
-
+#Dict conversion
 def parse_to_dict(s) -> dict:
 	d = {}
 	for k, v in re.findall(p, s):
 		d[k] = v
 	return d
-
+#Discord integration
 def rokbot(x, y):
 	data = { 
 		"content" : x + "\n" + y,
@@ -33,18 +33,18 @@ def rokbot(x, y):
 
 def main():
 	exec()
-	#first app
+	#First app
 	info1 = linecache.getline(log, 8)
 	url1 = parse_to_dict(info1)["url"]
 	addr1 = parse_to_dict(info1)["addr"]
-	#second app
+	#Second app
 	info2 = linecache.getline(log, 9)
 	url2 = parse_to_dict(info2)["url"]
 	addr2 = parse_to_dict(info2)["addr"]
-	#message vars 
+	#Message vars 
 	status1 = "Tunnel " + url1 + " is exposed to " + addr1
 	status2 = "Tunnel " + url2 + " is exposed to " + addr2
-	#integration if webhook exists
+	#Integration if webhook exists
 	if discord_url != "":
 		rokbot(status1, status2)
 main()
