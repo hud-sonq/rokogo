3de9aaee67c9eda7212a2b2c24667f5a80716b99
Hudson Q <hudson@koipond.xyz>
2023-02-17 19:44:15 -0600

    testing config loop

--- rokogo2.py
+++ rokogo2.py
@@ -59,20 +59,19 @@ def rokbot(x, y):
 
 def main():
 	makeconfig()
-	
-	# exec()
-	# #First app
-	# info1 = linecache.getline(log, 8)
-	# url1 = parse_to_dict(info1)["url"]
-	# addr1 = parse_to_dict(info1)["addr"]
-	# #Second app
-	# info2 = linecache.getline(log, 9)
-	# url2 = parse_to_dict(info2)["url"]
-	# addr2 = parse_to_dict(info2)["addr"]
-	# #Message vars 
-	# status1 = "Tunnel " + url1 + " is exposed to " + addr1
-	# status2 = "Tunnel " + url2 + " is exposed to " + addr2
-	# #Integration if webhook exists
-	# if discord_url != "":
-	# 	rokbot(status1, status2)
+	exec()
+	#First app
+	info1 = linecache.getline(log, 8)
+	url1 = parse_to_dict(info1)["url"]
+	addr1 = parse_to_dict(info1)["addr"]
+	#Second app
+	info2 = linecache.getline(log, 9)
+	url2 = parse_to_dict(info2)["url"]
+	addr2 = parse_to_dict(info2)["addr"]
+	#Message vars 
+	status1 = "Tunnel " + url1 + " is exposed to " + addr1
+	status2 = "Tunnel " + url2 + " is exposed to " + addr2
+	#Integration if webhook exists
+	if discord_url != "":
+		rokbot(status1, status2)
 main()
